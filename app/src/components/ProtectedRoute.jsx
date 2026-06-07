5ff86f619c9cc96dc81281245999fc6862742247
dong <vuvandong10022004@gmail.com>
2026-06-07 22:25:16 +0700

    componenets/ProtectedRoute

--- app/src/components/ProtectedRoute.jsx
+++ app/src/components/ProtectedRoute.jsx
@@ -1,10 +1,14 @@
+import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = () => {
-  return (
-    <>
-      ProtectedRoute
-    </>
-  )
-}
+const ProtectedRoute = ({ children }) => {
+    const user = localStorage.getItem('user');
+    const location = useLocation();
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
+};
 
 export default ProtectedRoute;
